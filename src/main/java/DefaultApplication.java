22b90034a00ba29332ef51297ea88c55e36934c8
Alex Hoff <AlexHoff1@users.noreply.github.com>
2021-12-27 20:47:41 -0800

    Median finding solution using interface

--- src/main/java/DefaultApplication.java
+++ src/main/java/DefaultApplication.java
@@ -14,15 +14,22 @@ public class DefaultApplication {
         StockPriceStatistics medianFinderOptimal = new NaiveStockPriceStatistics();
 
         List<Integer> integerStream = Arrays.asList(1,3,2,4,7,19,-1,5,12);
-        for(Integer element: integerStream) {
-            medianFinderNaive.insert(element);
-            medianFinderOptimal.insert(element);
-            log.info("After adding {} the median is {} and {}",
-                    element,
-                    medianFinderNaive.findMedian(),
-                    medianFinderOptimal.findMedian());
-        }
+        logResults(integerStream, medianFinderNaive);
+        logResults(integerStream, medianFinderOptimal);
+    }
 
+    public static void logResults(List<Integer> integers, StockPriceStatistics stockPriceStatistics) {
+        for(Integer element: integers) {
+            stockPriceStatistics.insert(element);
+            stockPriceStatistics.insert(element);
+            log.debug("After adding {} the median is {}",
+                    element,
+                    stockPriceStatistics.findMedian());
+        }
+        log.info("Final answer for the problem is low:{}, median:{}, high:{}",
+                stockPriceStatistics.findLow(),
+                stockPriceStatistics.findMedian(),
+                stockPriceStatistics.findHigh());
     }
 
 }
